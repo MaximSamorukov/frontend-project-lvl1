e882bd7e44ccc51cf84b5dbb93b2b495bc37a4df
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-02 21:02:34 +0300

    commit 5

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // import readlineSync from "readline-sync";
-import { cli } from "../src/cli.js";
-console.log("Welcome to the Brain Games!");
+import cli from '../src/cli';
+
+console.log('Welcome to the Brain Games!');
 cli();
